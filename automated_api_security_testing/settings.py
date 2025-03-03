a9690b5901f4be4e785eae071c38799b0cf1c8fc
sakilanasrinsetu <setusakilanasrin@gmail.com>
2025-03-03 03:14:37 -0600

    requirements file add

--- automated_api_security_testing/settings.py
+++ automated_api_security_testing/settings.py
@@ -30,6 +30,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+THIRD_PARTY_APPS = [
+    
+]
+
+LOCAL_APPS = [
+    'api_security',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + THIRD_PARTY_APPS + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

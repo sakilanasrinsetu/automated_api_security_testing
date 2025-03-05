8773c903ebde9f9ec83ce6a17f252d6a83182e79
sakilanasrinsetu <setusakilanasrin@gmail.com>
2025-03-05 02:56:02 -0600

    App URL

--- api_security/urls.py
+++ api_security/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import *
+from api_security.views import MITREAttackTacticViewSet
 
 urlpatterns =[ 
     path('mitre_attack_active/',
          MITREAttackTacticViewSet.as_view({'get': 'list', 'post':'create'}, name='mitre_attack_active')),
+    
     path('mitre_attack_active/<pk>/',
          MITREAttackTacticViewSet.as_view({'delete': 'destroy',
                               "patch":"update", "get":"retrieve"},
--- automated_api_security_testing/urls.py
+++ automated_api_security_testing/urls.py
@@ -1,69 +1,41 @@
-"""
-URL configuration for automated_api_security_testing project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.urls import path, include,  re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
-from rest_framework import permissions
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
-from rest_framework import routers
 
-
-
-router = routers.DefaultRouter()
-import debug_toolbar
 
 schema_urlpatterns = [
     path("spectacular/", SpectacularAPIView.as_view(), name="schema"),
-    path("",SpectacularSwaggerView.as_view(url_name="schema"),name="doc",),
-    path("redoc/", SpectacularRedocView.as_view(url_name="schema"),name="redoc",),
-    
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="doc"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 
-url_patterns = [
+app_url_patterns = [
     path('user_account/', include('user.urls')),
     path('api_security/', include('api_security.urls')),
-    
 ]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('admin/', include('rest_framework.urls')),
-    path('', include([path('/', include(url_patterns))])),
+    path('', include(app_url_patterns)),
 ] + schema_urlpatterns
 
 
 if settings.DEBUG:
-    urlpatterns.append(
-        path("__debug__/", include("debug_toolbar.urls")),
-    )  
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
+
 
 if settings.DEBUG:
-    urlpatterns = urlpatterns + \
-                  static(settings.STATIC_URL,
-                         document_root=settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + \
-                  static(settings.MEDIA_URL,
-                         document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, 
+                          document_root=settings.MEDIA_ROOT)
